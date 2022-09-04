0a06c6571165233848b5262056887c3055b9749b
MKish420 <meaghanmpeters@hotmail.com>
2022-09-04 19:26:58 -0400

    added search icon style to search bar added cursor

--- src/components/Header.js
+++ src/components/Header.js
@@ -60,9 +60,44 @@ const Logo = styled.span`
     font-size: 0px;
 `;
 
-const Search = styled.div``;
+const Search = styled.div`
+    opacity: 1;
+    flex-grow: 1;
+    position: relative;
+    & > div {
+        max-width: 280px;
+        input {
+            border: none;
+            box-shadow: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            color: rgba(0, 0, 0, 0.9);
+            width: 218px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+            font-weight: 400;
+            font-size: 14px;
+            height: 34px;
+            border-color: #dce6f1;
+            vertical-align: text-top;
 
-const SearchIcon = styled.div``;
+         }
+    }
+          `;
+
+const SearchIcon = styled.div`
+    width: 40px;
+    position: absolute;
+    z-index: 1;
+    top: 10px;
+    left: 2px;
+    border-radius: 0 2px 2px 0;
+    margin: 0;
+    pointer-events: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `;
 
 
 export default Header;
