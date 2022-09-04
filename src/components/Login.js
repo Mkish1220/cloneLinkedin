190b0e9014954529ef90ff31ef269b8194128a64
MKish420 <meaghanmpeters@hotmail.com>
2022-09-04 17:39:50 -0400

    added google button with hover option and box shading

--- src/components/Login.js
+++ src/components/Login.js
@@ -137,8 +137,31 @@ img {
 `;
 
 const Form = styled.div`
+margin-top: 100px;
+width: 408px;
+@media (max-width: 768px) {
+    margin-top: 20px;
+    } 
 `;
 
-const Google = styled.button``;
+const Google = styled.button`
+display: flex;
+justify-content: center;
+background-color: #fff;
+align-items: center;
+height: 56px;
+width: 100%;
+border-radius: 28px;
+box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6), inset 0 0 0 2px rgba(0, 0, 0, 0.6), inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+vertical-align: middle;
+z-index: 0;
+transition-duration: 167ms;
+font-size: 20px;
+color: rgba(0, 0, 0, 0.6);
+    &:hover {
+        background-color: rgba(207, 207, 207, 0.25);
+        color: rgba(0, 0, 0, 0.75);
+    }
+`;
 
 export default Login;
