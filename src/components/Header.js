5ecfc09a10ba53699dd6206b9a8c4da189c501ee
MKish420 <meaghanmpeters@hotmail.com>
2022-09-04 19:19:34 -0400

    added search bar with search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,28 @@ const Header = (props) => {
                         <img src="images/home-logo.svg" alt="" />
                     </a>
                 </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search" />
+                    </div>
+                    <SearchIcon>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 24 24" 
+                         fill="none" 
+                         stroke="currentColor"
+                         width="24px"
+                         height="14px"
+                          stroke-width="2" 
+                          stroke-linecap="round" 
+                          stroke-linejoin="round"
+                           class="search-icon">
+                            <circle cx="11" cy="11" r="8">
+                                </circle>
+                                <line x1="21" y1="21" x2="16.65" y2="16.65">
+                                    </line>
+                                    </svg>
+                    </SearchIcon>
+                </Search>
             </Content>
         </Container >
     );
@@ -37,6 +59,10 @@ const Logo = styled.span`
     margin-right: 8px;
     font-size: 0px;
 `;
+
+const Search = styled.div``;
+
+const SearchIcon = styled.div``;
 
 
 export default Header;
